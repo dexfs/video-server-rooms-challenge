8fe74df13a9835bf152ee4dbe3068bef35cc24c9
André Santos <andre@dexdev.com.br>
2020-10-30 19:34:23 -0300

    Setup Project

---
+++ ormconfig.ts
@@ -0,0 +1,47 @@
+// import { ConnectionOptions } from 'typeorm';
+import { loadEnv } from './src/bootstrap/loadEnvironments';
+
+loadEnv();
+
+const entities = ['./src/modules/**/entities/*.ts'];
+const migrations = ['./src/database/migrations/*.ts'];
+const factories = ['src/database/factories/**/*{.ts,.js}'];
+const seeds = ['src/database/seeds/**/*{.ts,.js}'];
+
+const connectionOptions = [
+  {
+    name: 'development',
+    type: 'postgres',
+    database: String(process.env.TYPEORM_DATABASE),
+    host: String(process.env.TYPEORM_HOST),
+    port: Number(process.env.TYPEORM_PORT),
+    username: String(process.env.TYPEORM_USERNAME),
+    password: String(process.env.TYPEORM_PASSWORD),
+    synchronize: false,
+    logging: true,
+    entities,
+    migrations,
+    factories,
+    seeds,
+    cli: {
+      migrationsDir: './src/database/migrations',
+      entitiesDir: './src/modules/**/entities/*.ts',
+    },
+    default: true
+    // dropSchema: true
+  },
+  {
+    name: 'test',
+    type: 'sqlite',
+    database: ':memory:',
+    synchronize: true,
+    logging: false,
+    entities,
+    migrations,
+    factories,
+    seeds,
+    dropSchema: true,
+  },
+];
+
+module.exports = connectionOptions;
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { validate } from 'express-validation';
+
+import IndexController from '@app/http/auth/controllers/IndexController';
+import authValidation from '@app/http/requests/auth.request';
+
+const authRouter = Router();
+
+authRouter.post(
+  '/',
+  validate(authValidation, {}, {}),
+  IndexController.authenticate,
+);
+
+authRouter.get(
+  '/',
+  function(req, res) {
+    return res.json({data: 'ok'})
+  }
+);
+
+
+export default authRouter;
