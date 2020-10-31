e660ecbf2797cc44266edd154adc5adf2e823912
André Santos <andre@dexdev.com.br>
2020-10-31 14:39:15 -0300

    chore: update ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,9 +1,8 @@
-// import { ConnectionOptions } from 'typeorm';
 import { loadEnv } from './src/bootstrap/loadEnvironments';
 
 loadEnv();
 
-const entities = ['./src/modules/**/entities/*.ts'];
+const entities = ['./src/app/entities/*.ts'];
 const migrations = ['./src/database/migrations/*.ts'];
 const factories = ['src/database/factories/**/*{.ts,.js}'];
 const seeds = ['src/database/seeds/**/*{.ts,.js}'];
@@ -25,7 +24,7 @@ const connectionOptions = [
     seeds,
     cli: {
       migrationsDir: './src/database/migrations',
-      entitiesDir: './src/modules/**/entities/*.ts',
+      entitiesDir: './src/app/entities/*.ts',
     },
     default: true
     // dropSchema: true
