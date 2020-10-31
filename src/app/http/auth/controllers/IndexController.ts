5fa9b87e44d66b8250f42d17e32cddf3eb0f756b
André Santos <andre@dexdev.com.br>
2020-10-31 12:56:40 -0300

    feat: auth controller

--- src/app/http/auth/controllers/IndexController.ts
+++ src/app/http/auth/controllers/IndexController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express';
-// import AuthenticateAction from '../actions/AuthenticateAction';
+import AuthenticateAction from '@app/actions/AuthenticateAction';
 
 class IndexController {
   static async authenticate(request: Request, response: Response) {
-    // const authenticateAction = new AuthenticateAction();
-    // const { user, token } = await authenticateAction.execute(request.body);
-    // return response.json({ user, token });
-    return response.json({ data: 'ok' });
+    const authenticateAction = new AuthenticateAction();
+    const { user, token } = await authenticateAction.execute(request.body);
+    return response.json({ user, token });
   }
 }
 
